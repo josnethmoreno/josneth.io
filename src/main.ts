bf32ece1a86ae5a16fe18bfad938674e3de56508
Josneth Moreno <josnethmoreno@gmail.com>
2023-04-20 12:04:47 -0400

    nav toggle

--- src/main.ts
+++ src/main.ts
@@ -1,2 +1,15 @@
 import './style.css'
 import './reset.css'
+
+const toggle = document.getElementById('toggle')
+const theme = document.getElementById('theme')
+const nav = document.getElementById('nav')
+
+const toggleNav = () => {
+  const show = (nav?.getAttribute('data-show') === 'true')
+  if(!show) return nav?.setAttribute('data-show', 'true')
+  if(show) return nav?.setAttribute('data-show', 'false')
+}
+
+toggle?.addEventListener('click', toggleNav)
+
